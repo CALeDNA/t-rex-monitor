a447090395d39db6b387be4e99a3dc58646953fb
hector-baez <hbaez@oxy.edu>
2023-10-30 18:18:10 -0500

    fix yaml -> yml

--- grafana/main/main_grafana_setup.sh
+++ grafana/main/main_grafana_setup.sh
@@ -100,7 +100,7 @@ sudo mv loki-linux-amd64 /usr/local/bin/loki
 loki --version
 
 sudo mkdir -p /data/loki
-sudo cp loki-local-config.yaml /etc/loki-local-config.yaml
+sudo cp loki-local-config.yml /etc/loki-local-config.yml
 sudo cp loki.service /etc/systemd/system/loki.service
 
 curl -O -L "https://github.com/grafana/loki/releases/download/v$PROMTAIL_VERSION/promtail-linux-amd64.zip"
